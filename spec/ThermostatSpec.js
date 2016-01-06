2d4fb284fb2751cd4be0eabb780c083fdcabaf32
Kirk Smith <kirks@hotmail.co.uk>
2016-01-06 15:36:00 +0000

    set min temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,7 @@
+(function(){
+  'use strict';
+}());
+
 describe('Thermostat', function() {
   var thermostat;
 });
@@ -10,7 +14,23 @@ it('has a default temperature of 20', function() {
   expect(thermostat.temp).toEqual(20);
 });
 
+it('returns current temp', function() {
+  expect(thermostat.getTemp()).toEqual(thermostat.temp);
+});
+
 it('increases temp when button is pressed', function() {
-  thermostat.increaseTemp(30);
-  expect(thermostat.temp).toEqual(30);
+  thermostat.increaseTemp(1);
+  expect(thermostat.getTemp()).toEqual(21);
+});
+
+it('button decreases temp', function(){
+  thermostat.decreseTemp(1);
+  expect(thermostat.getTemp()).toEqual(19);
+});
+
+it('min temp cannot be below 10', function(){
+  for(var i = 0; i < 11; i ++){
+    thermostat.decreseTemp();
+  }
+  expect(thermostat.getTemp()).toEqual(10)
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,10 +1,29 @@
+(function(){
+  'use strict';
+}());
+
 function Thermostat() {
-  "use strict";
   this.temp = DEFAULT_TEMP;
+  this.MIN_TEMP = 10;
 }
 
 const DEFAULT_TEMP = 20;
 
-Thermostat.prototype.increaseTemp = function (SetTemp) {
-  this.temp = SetTemp;
+Thermostat.prototype.getTemp = function() {
+  return this.temp;
+};
+
+Thermostat.prototype.isMinTemp = function(){
+  return this.temp === this.MIN_TEMP;
+};
+
+Thermostat.prototype.increaseTemp = function() {
+  this.temp += 1;
+};
+
+Thermostat.prototype.decreseTemp = function(){
+  if (this.isMinTemp()) {
+    return;
+  }
+  this.temp -= 1;
 };
